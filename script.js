c1e2f4f6a93bd8fa7b534dd418a1b6e1e360129c
crymory <141661302+crymory@users.noreply.github.com>
2024-05-19 14:33:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,124 @@
-// Обработчик нажатия на кнопку "Низкий"
+// 簡単計算モードと詳細計算モードどちらを選択しているかのフラグ
+let calcMethodType = "easy";
+
+// 開始ボタン押下時処理
+document.querySelector(".start-button").addEventListener("click", () => {
+  // 計算の元となるベース感度を取得
+  const baseSens = document.querySelector(".base-sens-input").value;
+  // 簡単計算モードと詳細計算モードのラジオボタンを取得
+  let calcMethod = document.querySelectorAll(".calc-mode-radio");
+  console.log(calcMethod);
+
+  // 簡単計算モードと詳細計算モードどちらを選択しているかを取得
+  for (let i = 0; i < calcMethod.length; i++) {
+    if (calcMethod[i].checked) {
+      calcMethodType = calcMethod[i].value;
+      console.log(calcMethodType);
+    }
+  }
+
+  // ベース感度空欄チェック
+  if (baseSens == "") {
+    alert("ベース感度を入力してください");
+    return;
+  }
+
+  const tbody = document.querySelector(".table tbody");
+  // 開始ボタン押下で履歴リセット
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  const tr = document.createElement("tr");
+  let high = document.createElement("td");
+  let middle = document.createElement("td");
+  let low = document.createElement("td");
+
+  if (calcMethodType == "easy") {
+    high.innerText = Math.round(baseSens * 1.5 * 100) / 100;
+    middle.innerText = baseSens * 1;
+    low.innerText = Math.round(baseSens * 0.5 * 100) / 100;
+  } else if (calcMethodType == "detailed") {
+    high.innerText = Math.round(baseSens * 1.5 * 1000) / 1000;
+    middle.innerText = baseSens * 1;
+    low.innerText = Math.round(baseSens * 0.5 * 1000) / 1000;
+  }
+
+  tr.appendChild(high);
+  tr.appendChild(middle);
+  tr.appendChild(low);
+
+  tbody.appendChild(tr);
+});
+
+// ローボタン押下時処理
 document.querySelector(".low-button").addEventListener("click", () => {
-  const lastRow = document.querySelector(".table tbody").lastElementChild;
-  if (!lastRow) {
-    alert("Начните сначала, чтобы добавить новую строку");
-    return;
-  }
+  const middle = document.querySelector(".table tbody").lastElementChild.children[1];
+  const low = document.querySelector(".table tbody").lastElementChild.children[2];
 
-  const highText = lastRow.children[0].innerText;
-  const middleText = lastRow.children[1].innerText;
+  const newHigh = document.createElement("td");
+  newHigh.innerText = middle.innerText;
 
-  // Проверяем, что данные в ячейках можно преобразовать в числа
-  if (isNaN(parseFloat(highText)) || isNaN(parseFloat(middleText))) {
-    alert("Ошибка при получении данных из предыдущей строки");
-    return;
+  const newMiddle = document.createElement("td");
+  if (calcMethodType == "easy") {
+    newMiddle.innerText = Math.round(((Number(low.innerText) + Number(middle.innerText)) / 2) * 100) / 100;
+  } else if (calcMethodType == "detailed") {
+    newMiddle.innerText = Math.round(((Number(low.innerText) + Number(middle.innerText)) / 2) * 1000) / 1000;
   }
 
   const newLow = document.createElement("td");
-  newLow.innerText = middleText;
+  newLow.innerText = low.innerText;
 
-  const newMiddle = document.createElement("td");
-  if (calcMethodType == "простой") {
-    newMiddle.innerText = Math.round(((parseFloat(middleText) + parseFloat(highText)) / 2) * 100) / 100;
-  } else if (calcMethodType == "подробный") {
-    newMiddle.innerText = Math.round(((parseFloat(middleText) + parseFloat(highText)) / 2) * 1000) / 1000;
-  }
-
-  const newHigh = document.createElement("td");
-  newHigh.innerText = highText;
-
+  const tbody = document.querySelector(".table tbody");
   const tr = document.createElement("tr");
+
   tr.appendChild(newHigh);
   tr.appendChild(newMiddle);
   tr.appendChild(newLow);
 
-  const tbody = document.querySelector(".table tbody");
   tbody.appendChild(tr);
 
-  calculationCheck(newHigh.innerText, newMiddle.innerText, newMiddle);
+  calculationCheck(newMiddle.innerText, newLow.innerText, newMiddle);
 });
 
-// Обработчик нажатия на кнопку "Высокий"
+// ハイボタン押下時処理
 document.querySelector(".high-button").addEventListener("click", () => {
-  const lastRow = document.querySelector(".table tbody").lastElementChild;
-  if (!lastRow) {
-    alert("Начните сначала, чтобы добавить новую строку");
-    return;
-  }
-
-  const lowText = lastRow.children[2].innerText;
-  const middleText = lastRow.children[1].innerText;
-
-  // Проверяем, что данные в ячейках можно преобразовать в числа
-  if (isNaN(parseFloat(lowText)) || isNaN(parseFloat(middleText))) {
-    alert("Ошибка при получении данных из предыдущей строки");
-    return;
-  }
+  const high = document.querySelector(".table tbody").lastElementChild.children[0];
+  const middle = document.querySelector(".table tbody").lastElementChild.children[1];
 
   const newMiddle = document.createElement("td");
-  if (calcMethodType == "простой") {
-    newMiddle.innerText = Math.round(((parseFloat(middleText) + parseFloat(lowText)) / 2) * 100) / 100;
-  } else if (calcMethodType == "подробный") {
-    newMiddle.innerText = Math.round(((parseFloat(middleText) + parseFloat(lowText)) / 2) * 1000) / 1000;
+  if (calcMethodType == "easy") {
+    newMiddle.innerText = Math.round(((Number(middle.innerText) + Number(high.innerText)) / 2) * 100) / 100;
+  } else if (calcMethodType == "detailed") {
+    newMiddle.innerText = Math.round(((Number(middle.innerText) + Number(high.innerText)) / 2) * 1000) / 1000;
   }
 
   const newLow = document.createElement("td");
-  newLow.innerText = lowText;
+  newLow.innerText = middle.innerText;
 
   const newHigh = document.createElement("td");
-  newHigh.innerText = middleText;
+  newHigh.innerText = high.innerText;
 
+  const tbody = document.querySelector(".table tbody");
   const tr = document.createElement("tr");
+
   tr.appendChild(newHigh);
   tr.appendChild(newMiddle);
   tr.appendChild(newLow);
 
-  const tbody = document.querySelector(".table tbody");
   tbody.appendChild(tr);
 
   calculationCheck(newHigh.innerText, newMiddle.innerText, newMiddle);
 });
+
+const calculationCheck = (a, b, newMiddle) => {
+  if (calcMethodType == "easy") {
+    if (Math.round((Number(a) - Number(b)) * 100) / 100 <= 0.01) {
+      alert(`あなたに最適な感度は${newMiddle.innerText}です`);
+    }
+  } else if (calcMethodType == "detailed") {
+    if (Math.round((Number(a) - Number(b)) * 1000) / 1000 <= 0.001) {
+      alert(`あなたに最適な感度は${newMiddle.innerText}です`);
+    }
+  }
+};
