4f1adcad74a60e1b884c2b7acd317fcad52909af
crymory <141661302+crymory@users.noreply.github.com>
2024-05-19 12:21:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-function calculateDPI() {
+function calculateSettings() {
     const width = parseFloat(document.getElementById('width').value);
     const height = parseFloat(document.getElementById('height').value);
     const diagonal = parseFloat(document.getElementById('diagonal').value);
+    const sensitivity = parseFloat(document.getElementById('sensitivity').value);
 
-    if (!width || !height || !diagonal) {
+    if (!width || !height || !diagonal || !sensitivity) {
         alert("Пожалуйста, введите все значения.");
         return;
     }
 
     const diagonalPixels = Math.sqrt(width * width + height * height);
     const dpi = diagonalPixels / diagonal;
+    const sensDPI = sensitivity * dpi;
 
-    document.getElementById('result').innerText = `DPI: ${dpi.toFixed(2)}`;
+    document.getElementById('resultDPI').innerText = `DPI: ${dpi.toFixed(2)}`;
+
+    const resultsBody = document.getElementById('resultsBody');
+    const newRow = document.createElement('tr');
+    
+    newRow.innerHTML = `
+        <td>${dpi.toFixed(2)}</td>
+        <td>${sensitivity.toFixed(2)}</td>
+        <td>${sensDPI.toFixed(2)}</td>
+    `;
+
+    resultsBody.appendChild(newRow);
 }
